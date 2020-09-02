3e906db7b5ad27c4d7974f07406b4d254ae5bc41
Yujin <ougenie02@gmail.com>
2020-09-02 14:53:19 +0900

    2nd

--- 1003_피보나치 함수.cpp
+++ 1003_피보나치 함수.cpp
@@ -3,44 +3,52 @@
 
 using namespace std;
 
-int zero = 0;
-int one = 0;
+typedef struct call {
+	int callZero;
+	int callOne;
+}call;
 
-vector<int> arr;
+vector<call> fibo;
 
-//시간초과
-int fibo(int n)
-{
-	if (n == 0)
-	{
-		zero++;
-		return 0;
-	}
-
-	if (n == 1)
-	{
-		one++;
-		return 1;
-	}
-
-	return fibo(n-1) + fibo(n-2);
-}
 int main()
 {
-	int testCase;
+	int t;
+	int n;
 	
-	cin >> testCase;
+	cin >> t;
 
-	for (int i = 0; i < testCase; i++)
+	fibo.push_back({ 1,0 });
+	fibo.push_back({ 0,1 });
+
+	for (int i = 0; i < t; i++)
 	{
-		int num;
-		cin >> num;
+		cin >> n;
 
-		fibo(num);
+		if (fibo.size() - 1 >= n)
+		{
+			cout << fibo[n].callZero << " " << fibo[n].callOne << "\n";
+		}
+		else
+		{
+			int now = fibo.size() - 1;
+			while (1)
+			{
+				int callZeroSum = fibo[now - 1].callZero + fibo[now].callZero;
+				int callOneSum = fibo[now - 1].callOne + fibo[now].callOne;
 
-		cout << zero << " " << one << "\n";
+				fibo.push_back({ callZeroSum,callOneSum });
+				
+				now += 1;
 
-		zero = 0;
-		one = 0;
+				if (now == n)
+				{
+					cout << fibo[n].callZero << " " << fibo[n].callOne << "\n";
+					break;
+				}
+			}
+			
+
+		}
+
 	}
 }
