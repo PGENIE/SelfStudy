47aafd49497f2c8e9b0d6a6053d325e779b84260
Yujin <ougenie02@gmail.com>
2020-09-10 23:46:56 +0900

    1st

---
+++ 5086_배수와 약수.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int a, b;
+
+	
+	while(cin >> a >> b)
+	{
+		if (a == 0 && b == 0)
+			break;
+
+		if (b % a == 0)
+			cout << "factor";
+		else if (a % b == 0)
+			cout << "multiple";
+		else
+			cout << "neither";
+
+		cout << "\n";
+	}
+
+	return 0;
+}
