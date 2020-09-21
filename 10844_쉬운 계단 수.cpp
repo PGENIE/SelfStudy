bc21217263f66d3bd0063c49cb41f90d6bc9630e
Yujin <ougenie02@gmail.com>
2020-09-22 00:16:58 +0900

    2nd. 출력값 잘 확인하고 출력하기

--- 10844_쉬운 계단 수.cpp
+++ 10844_쉬운 계단 수.cpp
@@ -18,25 +18,22 @@ int main()
 
 	for (int i = 2; i <= n; i++)
 	{
-		for(int j = 0 ; j <= 9 ;j ++)
+		dp[i][0] = dp[i - 1][1] % 1000000000;
+		dp[i][9] = dp[i - 1][8] % 1000000000;
+		for(int j = 1 ; j < 9 ;j ++)
 		{
-			if (j == 0)
-				dp[i][j] = dp[i - 1][j + 1]%1000000000;
-			else if (j == 9)
-				dp[i][j] = (dp[i - 1][j - 1]) % 1000000000;
-			else
-				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
 		}
 	}
 
 	long long result = 0;
 	for (int i = 0; i <= 9; i++)
 	{
-		cout << dp[n][i]<<"\n";
 		result += dp[n][i];
 	}
 
-	cout << result;
+	cout << result% 1000000000 << "\n";
+
 	return 0;
 
 }
