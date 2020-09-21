40c7d18a104f76af6ed10b203dd3b58c1335e470
Yujin <ougenie02@gmail.com>
2020-09-21 23:58:28 +0900

    1st try

---
+++ 10844_쉬운 계단 수.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int n;
+long long dp[101][10];
+
+int main()
+{
+	cin >> n;
+
+	dp[1][0] = 0;
+	for (int i = 1; i < 10; i++)
+	{
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++)
+	{
+		for(int j = 0 ; j <= 9 ;j ++)
+		{
+			if (j == 0)
+				dp[i][j] = dp[i - 1][j + 1]%1000000000;
+			else if (j == 9)
+				dp[i][j] = (dp[i - 1][j - 1]) % 1000000000;
+			else
+				dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1000000000;
+		}
+	}
+
+	long long result = 0;
+	for (int i = 0; i <= 9; i++)
+	{
+		cout << dp[n][i]<<"\n";
+		result += dp[n][i];
+	}
+
+	cout << result;
+	return 0;
+
+}
